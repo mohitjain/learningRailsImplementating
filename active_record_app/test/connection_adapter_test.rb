216eeff5353c683959b520789550b6dafc49dfb7
Mohit Jain <jain.mohit27@gmail.com>
2014-10-20 01:02:36 +0530

    test file and user class generated

--- active_record_app/test/connection_adapter_test.rb
+++ active_record_app/test/connection_adapter_test.rb
@@ -5,12 +5,12 @@ class ConnectionAdapterTest < Test::Unit::TestCase
   def setup
     @adapter = SqliteAdapter.new
   end
-  
+
   def test_execute
     row = @adapter.execute("SELECT * FROM users").first
     assert_equal({ id: 1, name: "Marc", 0 => 1, 1 => "Marc" }, row)
   end
-  
+
   def test_columns
     assert_equal [:id, :name], @adapter.columns("users")
   end
--- active_record_app/test/test_helper.rb
+++ active_record_app/test/test_helper.rb
@@ -1,4 +1,3 @@
 Dir.chdir File.expand_path("../../", __FILE__)
 $LOAD_PATH.unshift ".", "lib", "test"
-
 require "test/unit"
