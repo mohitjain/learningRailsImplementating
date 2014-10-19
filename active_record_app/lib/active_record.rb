1118cc7f322b5e5eec081e1e8adb56e2f47a82b1
Mohit Jain <jain.mohit27@gmail.com>
2014-10-20 01:18:27 +0530

    copy pasted this whole code into a module

---
+++ active_record_app/lib/active_record.rb
@@ -0,0 +1,31 @@
+module ActiveRecord
+  class base
+    @@connection = SqliteAdapter.new
+
+    def initialize(attributes)
+      @attributes = attributes
+    end
+
+    def id
+      @attributes[:id]
+    end
+
+    def name
+      @attributes[:name]
+    end
+
+
+    def self.find(id)
+      find_by_sql("select * from users where id=#{id} LIMIT 1").first
+    end
+
+    def self.find_by_sql(sql)
+      rows = @@connection.execute(sql)
+
+      rows.map do |attributes|
+        new attributes
+      end
+    end
+
+  end
+end
