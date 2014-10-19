549abe7e7444932944979c89c37371eacd8d0d04
Mohit Jain <jain.mohit27@gmail.com>
2014-10-20 01:17:07 +0530

    implemented find method in user class and now will be extracting this to a module

--- active_record_app/app/models/user.rb
+++ active_record_app/app/models/user.rb
@@ -1,4 +1,8 @@
+require 'connection_adapter'
+
 class User
+  @@connection = SqliteAdapter.new
+
   def initialize(attributes)
     @attributes = attributes
   end
@@ -9,5 +13,18 @@ class User
 
   def name
     @attributes[:name]
+  end
+
+
+  def self.find(id)
+    find_by_sql("select * from users where id=#{id} LIMIT 1").first
+  end
+
+  def self.find_by_sql(sql)
+    rows = @@connection.execute(sql)
+
+    rows.map do |attributes|
+      new attributes
+    end
   end
 end
--- active_record_app/test/user_test.rb
+++ active_record_app/test/user_test.rb
@@ -2,9 +2,20 @@ require File.dirname(__FILE__) + '/test_helper'
 require "app/models/user"
 
 class UserTest < Test::Unit::TestCase
-   def test_intialize_with_attributes
+  def test_intialize_with_attributes
     user = User.new(id: 1, name: "Marc")
     assert_equal 1, user.id
     assert_equal "Marc", user.name
   end
+
+  def test_find
+    user = User.find(1)
+    assert_equal 1, user.id
+    assert_equal "Marc", user.name
+
+    user = User.find(2)
+    assert_equal 2, user.id
+    assert_equal "Greg", user.name
+  end
 end
+
